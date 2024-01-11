9046f706b6a752a9a400d08ad515067cac52c7d8
zhack <zhacky@gmail.com>
2024-01-11 09:52:26 +0800

    Initial upload

---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+const styleguide = require('@vercel/style-guide/prettier');
+
+module.exports = {
+  ...styleguide,
+  plugins: [...styleguide.plugins, 'prettier-plugin-tailwindcss'],
+  "semi": true
+};
